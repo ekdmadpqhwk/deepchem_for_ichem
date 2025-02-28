a250bca5088de3c40a8b18356b363f6107eb47b5
spark <ekdmadpqhwk81@gmail.com>
2025-02-28 11:27:05 -0500

    IPAmap featurizer refactoring - v11

--- deepchem/feat/ichem_featurizers/IPA_map_featurizers.py
+++ deepchem/feat/ichem_featurizers/IPA_map_featurizers.py
@@ -348,7 +348,7 @@ class IPAMapFeaturizer(Featurizer):
         
         return edge_index, edge_features
 
-    def _featurize(self, multimol2_file_path, pose_id) -> GraphData:
+    def _featurize(self, datapoint: (multimol2_file_path, pose_id), **kwargs) -> GraphData:
         """Calculate pocket-ligand graph from iChem generated interaction pseudoatom map
 
         Returns
