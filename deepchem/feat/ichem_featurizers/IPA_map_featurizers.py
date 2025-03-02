88b8907777b4d7b4cc0753c6099a99f8f66e993d
spark <ekdmadpqhwk81@gmail.com>
2025-03-02 18:06:06 -0500

    IPAmap featurizer refactoring - v39

--- deepchem/feat/ichem_featurizers/IPA_map_featurizers.py
+++ deepchem/feat/ichem_featurizers/IPA_map_featurizers.py
@@ -381,14 +381,8 @@ class IPAMapFeaturizer(IChemFeaturizer):
 
         # Create rdkit mol object from interaction and atom info
         rdkit_mol = self._modify_rdkit_mol(mol2_file_path, int_atom_idx, atom_info_dict, lig_dict, prot_dict, bsa_lig_dict, bsa_prot_dict) 
-
-        # Delete the mol2 file in temp dir
-        mol2_file_path.unlink()
         
         if rdkit_mol is None:
-
-            # Delete the mol2 file in temp dir
-            mol2_file_path.unlink()
             return None
             
         # Create distance matrix between all atom pairs
